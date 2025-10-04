d19983eb7fc71fd2dc164c21bbc8594fc7359575
Shashvat Mehta <shashvatsmehta@gmail.com>
2025-10-03 20:04:46 -0700

    Removed unnecessary turn functions
    Removed buggy methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Teleop.java
@@ -1,5 +1,5 @@
 //imports
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
