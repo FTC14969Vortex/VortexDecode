08ce79d913b72a8ca58be75dc67522977d39e366
alaqmarp <alaqmar0001@gmail.com>
2025-10-27 20:16:01 -0700

    Added distance sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Kicker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Kicker.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.Helper;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Kicker {
 
@@ -20,5 +25,17 @@ public class Kicker {
 
     public void setKickerPos(double position){
         kickerPos.setPosition(position);
+    }
+
+    private boolean readyToLoad() {
+        boolean ret = false;
+
+        // Get distance reading from 2M sensor
+        channelSersor = hardwareMap.get(DistanceSensor.class, "channelSensor");
+        double dDistance = channelSersor.getDistance(DistanceUnit.CM);
+        telemetry.addData("channel distance sensor", dDistance);
+        if (dDistance > 10 ) ret = true;
+
+        return ret;
     }
 }
