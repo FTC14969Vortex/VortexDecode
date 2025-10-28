382e0ebdc4b0a77856e00d4bff00c7e07122c2e3
alaqmarp <alaqmar0001@gmail.com>
2025-10-27 20:12:30 -0700

    b

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Kicker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Kicker.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Helper;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -9,7 +10,8 @@ public class Kicker {
 
     private Servo kickerPos;
     private OpMode opMode;
-
+    private DistanceSensor channelSersor;
+    
     public void init(HardwareMap hwMap) {
         kickerPos = hwMap.get(Servo.class, "kicker");
         this.opMode = opMode;
